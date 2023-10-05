cf55d01a36c3e5565ffcbf59dc3f5e2b4d940c92
ian.dover <ian.dover@skunova.com>
2023-10-05 21:07:16 +0000

    got the fitness per evaluation working

--- main/optimization_problems.py
+++ main/optimization_problems.py
@@ -301,13 +301,183 @@ def get_all_optimization_algorithm_fitness_per_problem_size_graphs() -> None:
     problem_types = ["FourPeaks", "OneMax", "FlipFlop"]
 
     iterations = 5
-    # for problem_type in problem_types:
-    #     optimization_algorithm_fitness_per_problem_size_comparison(problem_type, iterations)
+    for problem_type in problem_types:
+        optimization_algorithm_fitness_per_problem_size_comparison(
+            problem_type, iterations
+        )
 
     for problem_type in problem_types:
         get_optimization_algorithm_fitness_per_problem_size_graphs(problem_type)
 
 
+def optimization_algorithm_fitness_per_evaluation(
+    algorithm: str = "rhc", problem_type: str = "FourPeaks", iterations: int = 50
+) -> tuple[np.ndarray, float, np.ndarray]:
+    assert algorithm in [
+        "rhc",
+        "sa",
+        "ga",
+        "mimic",
+    ], "Algorithm must be in ['rhc', 'sa', 'ga', 'mimic']"
+    assert problem_type in [
+        "FourPeaks",
+        "OneMax",
+        "FlipFlop",
+    ], "Problem type must be in ['FourPeaks', 'OneMax', 'FlipFlop']"
+
+    max_attempts_to_ensure_iterations = iterations * 10
+
+    if problem_type == "FourPeaks":
+        fitness = mlrose.FourPeaks(t_pct=0.1)
+    elif problem_type == "OneMax":
+        fitness = mlrose.OneMax()
+    elif problem_type == "FlipFlop":
+        fitness = mlrose.FlipFlop()
+
+    problem = mlrose.DiscreteOpt(
+        length=100, fitness_fn=fitness, maximize=True, max_val=2
+    )
+
+    if algorithm == "rhc":
+        best_state, best_fitness, fitness_curve = mlrose.random_hill_climb(
+            problem,
+            max_attempts=max_attempts_to_ensure_iterations,
+            max_iters=iterations,
+            restarts=0,
+            init_state=None,
+            curve=True,
+        )
+    elif algorithm == "sa":
+        schedule = mlrose.GeomDecay(init_temp=1.0, decay=0.95, min_temp=0.001)
+        best_state, best_fitness, fitness_curve = mlrose.simulated_annealing(
+            problem,
+            schedule=schedule,
+            max_attempts=max_attempts_to_ensure_iterations,
+            max_iters=iterations,
+            init_state=None,
+            curve=True,
+        )
+    elif algorithm == "ga":
+        best_state, best_fitness, fitness_curve = mlrose.genetic_alg(
+            problem,
+            pop_size=200,
+            mutation_prob=0.1,
+            max_attempts=max_attempts_to_ensure_iterations,
+            max_iters=iterations,
+            curve=True,
+        )
+    elif algorithm == "mimic":
+        best_state, best_fitness, fitness_curve = mlrose.mimic(
+            problem,
+            pop_size=20,
+            keep_pct=0.2,
+            max_attempts=max_attempts_to_ensure_iterations,
+            max_iters=iterations,
+            curve=True,
+        )
+
+    return (best_state, best_fitness, fitness_curve)
+
+
+def get_optimization_algorithm_fitness_per_evaluation_comparison(
+    problem_type: str = "FourPeaks",
+    iterations: int = 20,
+    output_location: str = "../outputs/optimization_algorithms/",
+    verbose: bool = True,
+) -> None:
+    assert problem_type in [
+        "FourPeaks",
+        "OneMax",
+        "FlipFlop",
+    ], "Problem type must be in ['FourPeaks', 'OneMax', 'FlipFlop']"
+
+    algorithms = [
+        "rhc",
+        "sa",
+        "ga",
+        "mimic",
+    ]
+    algorithm_evaluation_counts = [
+        rf"{algorithm}_evaluation_count" for algorithm in algorithms
+    ]
+    fitness_per_evaluation_list = []
+    evaluation_count_per_algorithm = []
+    for algorithm in algorithms:
+        _, best_fitness, curve = optimization_algorithm_fitness_per_evaluation(
+            algorithm, problem_type, iterations
+        )
+        if verbose == True:
+            print(rf"Finished running algorithm: {algorithm}.")
+
+        fitness_history = curve[:, 0].copy().reshape(-1, 1)
+        evaluation_count_history = curve[:, 1].copy().reshape(-1, 1)
+
+        fitness_per_evaluation_list.append(fitness_history)
+        evaluation_count_per_algorithm.append(evaluation_count_history)
+
+    fitness_per_evaluation_np = np.hstack(tuple(fitness_per_evaluation_list))
+    evaluation_count_per_algorithm_np = np.hstack(tuple(evaluation_count_per_algorithm))
+    fitness_per_evaluation_and_evaluation_count_per_algorithm_np = np.hstack(
+        (fitness_per_evaluation_np, evaluation_count_per_algorithm_np)
+    )
+    fitness_per_evaluation_and_evaluation_count_per_algorithm_df = pd.DataFrame(
+        fitness_per_evaluation_and_evaluation_count_per_algorithm_np,
+        columns=algorithms + algorithm_evaluation_counts,
+    )
+
+    fitness_per_evaluation_and_evaluation_count_per_algorithm_df.to_csv(
+        rf"{output_location}/all_algorithms_fitness_per_evaluation_{problem_type}",
+        index=False,
+    )
+
+
+def get_optimization_algorithm_fitness_per_evaluation_graphs(
+    problem_type: str = "FourPeaks",
+    input_location: str = "../outputs/optimization_algorithms/",
+    output_location: str = "../outputs/optimization_algorithms/",
+) -> None:
+    df = pd.read_csv(
+        rf"{input_location}/all_algorithms_fitness_per_evaluation_{problem_type}"
+    )
+
+    algorithms = [
+        "rhc",
+        "sa",
+        "ga",
+        "mimic",
+    ]
+    algorithm_evaluation_counts = [
+        rf"{algorithm}_evaluation_count" for algorithm in algorithms
+    ]
+
+    plt.figure(figsize=(10, 6))
+    for algorithm, algorithm_evaluation_count_field in zip(algorithms, algorithm_evaluation_counts):
+        sns.lineplot(data=df, x=algorithm_evaluation_count_field, y=algorithm, label=algorithm)
+
+    plt.xscale("log")
+
+    plt.title(rf"{problem_type}: Performance per Evaluation Count")
+    plt.xlabel("Evaluation Count")
+    plt.ylabel("Fitness")
+    plt.grid(True, which="both", ls="--", linewidth=0.5)
+    plt.tight_layout()
+    plt.legend()
+    plt.savefig(
+        rf"{output_location}/all_algorithms_fitness_per_evaluation_graph_{problem_type}.png"
+    )
+
+
+def get_all_optimization_algorithm_fitness_per_evaluation_graphs() -> None:
+    problem_types = ["FourPeaks", "OneMax", "FlipFlop"]
+
+    # for problem_type in problem_types:
+    #     get_optimization_algorithm_fitness_per_evaluation_comparison(problem_type)
+
+    for problem_type in problem_types:
+        get_optimization_algorithm_fitness_per_evaluation_graphs(problem_type)
+
+
 if __name__ == "__main__":
     # get_all_optimization_algorithm_fitness_per_iteration_graphs()
-    get_all_optimization_algorithm_fitness_per_problem_size_graphs()
+    # get_all_optimization_algorithm_fitness_per_problem_size_graphs()
+    get_all_optimization_algorithm_fitness_per_evaluation_graphs()
